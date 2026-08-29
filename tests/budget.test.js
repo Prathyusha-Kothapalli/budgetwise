8839fdc7db80fa8f2f17e16aaa8ff3bf9f9c41b7
Prathyusha-Kothapalli <prathyushakothapalli798@gmail.com>
2026-08-29 17:36:09 +0530

    Add automated unit tests for recurring items, limits, and chart data

---
+++ tests/budget.test.js
@@ -0,0 +1,20 @@
+﻿const RecurringManager = require('../js/recurringManager');
+const CategoryLimits = require('../js/categoryLimits');
+const BudgetCharts = require('../js/budgetCharts');
+
+describe('BudgetWise Unit Tests', () => {
+    test('RecurringManager sums monthly total', () => {
+        const rm = new RecurringManager();
+        rm.addRecurring('Salary', 5000, 'Income');
+        rm.addRecurring('Freelance', 1000, 'Income');
+        expect(rm.getMonthlyTotal()).toBe(6000);
+    });
+
+    test('CategoryLimits flags overspending', () => {
+        const cl = new CategoryLimits();
+        cl.setLimit('Dining', 200);
+        const res = cl.checkSpending('Dining', 250);
+        expect(res.exceeded).toBe(true);
+        expect(res.overAmount).toBe(50);
+    });
+});
